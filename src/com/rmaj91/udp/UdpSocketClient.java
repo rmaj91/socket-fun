45e218125639843fe3a8ceb517b753287f5b56b3
Rafal Maj <rmaj91@gmail.com>
2020-11-13 23:55:43 +0100

    Simple client/server

--- src/com/rmaj91/udp/UdpSocketClient.java
+++ src/com/rmaj91/udp/UdpSocketClient.java
@@ -1,8 +1,17 @@
 package com.rmaj91.udp;
 
+import java.io.IOException;
+import java.net.*;
+
 public class UdpSocketClient {
 
-    public static void main(String[] args) {
+    public static final int SEVER_PORT = 9999;
 
+    public static void main(String[] args) throws IOException {
+        DatagramSocket datagramSocket = new DatagramSocket();
+        String message = "message";
+        DatagramPacket datagramPacket = new DatagramPacket(message.getBytes(), 0, message.length(),
+                InetAddress.getLocalHost(), SEVER_PORT);
+        datagramSocket.send(datagramPacket);
     }
 }
--- src/com/rmaj91/udp/UdpSocketServer.java
+++ src/com/rmaj91/udp/UdpSocketServer.java
@@ -1,8 +1,23 @@
 package com.rmaj91.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UdpSocketServer {
 
-    public static void main(String[] args) {
+    public static final int PORT = 9999;
 
+    public static void main(String[] args) throws IOException {
+        DatagramSocket datagramSocket = new DatagramSocket(PORT);
+        byte[] buffer = new byte[256];
+        DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+        long start = System.nanoTime();
+        while (System.nanoTime() - start < 2_000_000_000_0L) {
+            datagramSocket.receive(datagramPacket);
+            byte[] data = datagramPacket.getData();
+            String message = new String(data);
+            System.out.println(message + ": " + datagramPacket.getPort());
+        }
     }
 }
