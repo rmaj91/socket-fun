fef7610b05e952993b498d2b9744a46ce62a3485
Rafal Maj <rmaj91@gmail.com>
2020-11-13 23:41:48 +0100

    init

---
+++ src/com/rmaj91/udp/UdpSocketClient.java
@@ -0,0 +1,8 @@
+package com.rmaj91.udp;
+
+public class UdpSocketClient {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/rmaj91/udp/UdpSocketServer.java
@@ -0,0 +1,8 @@
+package com.rmaj91.udp;
+
+public class UdpSocketServer {
+
+    public static void main(String[] args) {
+
+    }
+}
