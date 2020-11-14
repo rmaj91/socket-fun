e84d7002b94673fbfa96f09411928243eb82619f
Rafal Maj <rmaj91@gmail.com>
2020-11-14 17:50:53 +0100

    Add broadcast adress

--- src/com/rmaj91/Constants.java
+++ src/com/rmaj91/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int SERVER_PORT = 9999;
     public static final int MULTICAST_PORT = 9990;
     public static final String MULTICAST_GROUP = "225.4.5.6";
+    public static final String BROADCAST_GROUP = "225.255.255.255";
 }
